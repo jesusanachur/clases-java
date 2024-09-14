303ed405d9073881edc545aa8d8765aa6baea729
Jesús David Anachury Castro <167833141+jesusanachur@users.noreply.github.com>
2024-09-13 20:03:32 -0500

    Add files via upload

---
+++ Juego.java
@@ -0,0 +1,14 @@
+public class Juego {
+    private String nombre;
+    private int clave;
+    private double usuario;
+    private  int puntaje;
+
+    //contructor
+    public  juego(Strin nombre, int clave, double usuario, int puntaje) {
+        this.nombre=nombre;
+        this.clave=clave;
+        this.usuario=usuario;
+        this.puntaje=puntaje;
+    }
+
