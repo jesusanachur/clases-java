65e93865f94848db27ad8898752c5369c8561d48
Jesús David Anachury Castro <167833141+jesusanachur@users.noreply.github.com>
2024-09-13 20:03:06 -0500

    Add files via upload

---
+++ ejercicio.java
@@ -0,0 +1,25 @@
+public class ejercicio {
+    private String titulo;
+    private String autor;
+    private int isbn;
+    private date años;
+    private int  copia;
+
+
+    public ejercicio(String titulo, String autor, int isbn, date años, int copia ){
+        this.titulo=titulo;
+        this.autor=autor;
+        this.isbn=isbn;
+        this.años=años;
+        this.copia=copia;
+
+
+    }
+    public copia( int cantidad){
+        if copias(>0)
+            this.copia --;
+            system.out.println(" si se presta libro ")
+    }
+
+}
+
